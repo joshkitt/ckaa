e585fe9a2c4f3d5e97648403b7ac962a134feddf
Joshua Kitterman <joshkitterman@gmail.com>
2016-12-19 20:30:09 -0700

    init

---
+++ architect-utah.js
@@ -0,0 +1,42 @@
+function show1() {
+    $('#loading').hide();
+    $('#c1').fadeIn(1500, show2);
+}
+function show2() {
+    $('#c2').fadeIn(1500, show3);
+}
+function show3() {
+    $('#c3').fadeIn(1500, show4);
+}
+function show4() {
+    $('#c4').fadeIn(1500, show5);
+}
+function show5() {
+    $('#c5').fadeIn(1500, show6);
+}
+function show6() {
+    $('#c6').fadeIn(1500, show7);
+}
+function show7() {
+    $('#c7').fadeIn(1500, showContent);
+}
+function showContent() {
+    $('#content > img').fadeTo(1500, .1);
+    $('#main').fadeTo(1, .9);
+    $('#main').fadeIn(1500, showInfo);
+}
+function showInfo() {
+    $('#info').fadeIn(1500, showServices);
+}
+function showServices() {
+    $('#services').fadeIn(1500, showServices2);
+}
+function showServices2() {
+    $('#services2').fadeIn(1500, showLocation);
+}
+function showLocation() {
+    $('#location').fadeIn(1500, showAddress);
+}
+function showAddress() {
+    $('#address').fadeIn(1500);
+}
